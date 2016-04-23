0ab11800235402a3ae311d4593b14f8ed80aae89
roopkalam <roopkala@gmail.com>
2016-04-23 18:52:57 -0500

    Update OddEvenNumbers.java

--- OddEvenNumbers.java
+++ OddEvenNumbers.java
@@ -8,33 +8,42 @@ public class OddEvenNumbers {
 		int evenArray[] = new int[100];
 		int oddCount = 0;
 		int evenCount = 0;
+		int totalOdd = 0;
+		int totalEven = 0;
+		String newLine = System.getProperty("line.separator");
+		
 		for(int i=1; i<=100; i++)
 		{
 		  
 			if(i%2 == 0) 
 				{
 				evenArray[evenCount] = i;
+				totalEven = totalEven + i;
 				evenCount = evenCount + 1;
 				}
 			 else 
 				{
 				 oddArray[oddCount] = i;
+				 totalOdd = totalOdd + i;
 				 oddCount = oddCount + 1;
 				}
 		}	
 		//Print Even numbers
 		System.out.println("Even Numbers:");
-	   	for(int l=1;l<evenCount;l++)
+	   	for(int l=0;l<evenCount;l++)
 		{
 		System.out.print(evenArray[l]+", ");
 		}
+	   	System.out.println(newLine);
+	   	System.out.print("Total of evenArray: "+totalEven);
 		//Print Odd numbers
-	   	System.out.println("");
+	   	System.out.println(newLine);
 	   	System.out.println(""+"Odd Numbers:");
 		for (int l=0;l<oddCount;l++) 
 		{
 		System.out.print(oddArray[l]+", ");
 		}
-		
+		System.out.println(newLine);
+		System.out.print("Total of oddArray: "+totalOdd);
 	}
 }
